b22b4239fd228ef414d33023b7a61fe79d141cba
msdeshmukh009 <deshmukhmahesh009@gmail.com>
2021-07-26 17:47:53 +0530

    updated app.js

--- app.js
+++ app.js
@@ -22,46 +22,46 @@ function checkPalindrome(){
     var inputYear = bDateArr[0];
     var inputMonth = bDateArr[1];
     var inputDate = bDateArr[2];
-    let cheked = checkAllCombi(inputYear,inputMonth,inputDate);
+   var cheked = checkAllCombi(inputYear,inputMonth,inputDate);
 
     if(cheked){
-        outputMsgDiv.innerHTML="Whoa!!! Your birthdate in format",cheked, "is palindrome"
+        outputMsgDiv.innerText="Whoa!!! Your birthdate in format "+cheked+" is palindrome"
     }else{
-        var [nextdate,diff]=findNextDate(inputYear,inputMonth,inputDate);
-        // outputMsgDiv.innerHTML="`Awww! Your birthdate is not palindrome. Nearest palindrome date is "
-        // +{nextdate}+ "You missed it by " +{diff}+ " days."
-        outputMsgDiv.innerHTML= `Awww! Your birthdate is not palindrome. Nearest palindrome date is ${nextdate} You missed it by ${diff} days.`;
+        var [nextdate,diff]=findNextDate(inputDate,inputMonth,inputYear);
+        outputMsgDiv.innerText="`Awww! Your birthdate is not palindrome. Nearest palindrome date is "
+        +nextdate+ "You missed it by " +diff+ " days."
+        
     }
 }
 function checkAllCombi(yyyy,mm,dd){
     //yyyymmdd
     var dateFormat1 = yyyy+mm+dd;
-    // log(dateFormat1);
+
     //ddmmyyyy 
     var dateFormat2 = dd+mm+yyyy;
-    // log(dateFormat2);
+
      //mmddyy 
      var dateFormat3 = mm+dd+yyyy.substring(2);
-    //  log(dateFormat3);
+    
         
      //mddyyyy 
      var dateFormat4 = Number(mm)+dd+yyyy;
-    //  log(dateFormat4);
+    
     
      if(isPalindrome(dateFormat1)){
-         return (yyyy-mm-dd);
+            return (yyyy+"-"+mm+"-"+dd+" (yyyy-mm-dd)");
      }else if(isPalindrome(dateFormat2)){
-         return(dd-mm-yyyy);
+            return(dd+"-"+mm+"-"+yyyy+"(dd-mm-yyyy)");
      }else if(isPalindrome(dateFormat3)){
-         return(mm-dd-yyyy.substring(2));
+         return(mm+"-"+dd+"-"+yyyy.substring(2)+"(mm-dd-yy)");
      }else if(isPalindrome(dateFormat4)){
-         return(Number(mm)-dd-yyyy);
+         return(Number(mm)+"-"+dd+"-"+yyyy+"(m-dd-yyyy)");
      }else{
          return null;
      }
 }
 function isPalindrome(combistring){
-var mid = parseInt(combistring/2);
+var mid = Math.floor(combistring.length/2);
 for(var i =0;i<mid;i++){
     if(combistring[i]!=combistring[combistring.length-1-i])
     return false;
@@ -72,10 +72,10 @@ function findNextDate(date,month,year){
     var ddNo1 = Number(date);
     var mmNo1 = Number(month);
     var yyNo1 = Number(year);
-    let ddNo2= Number(date);
-    let mmNo2= Number(month);
-    let yyNo2=Number(year);
-    
+   var ddNo2= Number(date);
+   var mmNo2= Number(month);
+   var yyNo2=Number(year);
+ 
     for(var i=1;i>0;i++){
 
         //forward check
@@ -99,9 +99,12 @@ function findNextDate(date,month,year){
         ddString="0"+ddString;
     }
     var dateFound = checkAllCombi(yystring,mmString,ddString)
+    
     if(dateFound){
-        return[dateFound,i]
+        log(dateFound)
+        return ([dateFound,i])
     }
+    
 
       //backward check
       if(yyNo2>1){
@@ -117,18 +120,18 @@ function findNextDate(date,month,year){
                 ddNo2 = datesInMonth[mmNo2-1];
             }
         }
-        let yyString = yyNo2.toString();
-        let mmString = mmNo2.toString();
-        let ddString = ddNo2.toString();
+       var yyString = yyNo2.toString();
+       var mmString = mmNo2.toString();
+       var ddString = ddNo2.toString();
         if(mmString.length==1){
             mmString="0"+mmString;
         }
         if(ddString.length==1){
             ddString="0"+ddString;
         }
-        let dateFound = checkAllCombi(yyString, mmString, ddString);
+       var dateFound = checkAllCombi(yyString, mmString, ddString);
         if(dateFound){
-            return [dateFound, i];
+            return ([dateFound, i]);
         }
     }
 
